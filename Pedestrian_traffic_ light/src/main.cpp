7d540c28636475afb27ac1ace600e43e6566defc
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-03-14 13:15:26 +0100

    Update main.cpp

--- Pedestrian_traffic_ light/src/main.cpp
+++ Pedestrian_traffic_ light/src/main.cpp
@@ -3,14 +3,14 @@ int ampelRotPin = 12;
 int ampelGelbPin = 11;
 int ampelGruenPin = 10;
 int fgRotPin = 9;
-int fgRotPin = 8;
+int fgGruenPin = 8;
 
 void setup() {
   pinMode(ampelRotPin, OUTPUT);
   pinMode(ampelGelbPin, OUTPUT);
   pinMode(ampelGruenPin, OUTPUT);
   pinMode(fgRotPin, OUTPUT);
-  pinMode(fgRotPin, OUTPUT);
+  pinMode(fgGruenPin, OUTPUT);
 }
 
 void loop() {
@@ -20,26 +20,26 @@ void loop() {
   digitalWrite(ampelGruenPin, HIGH);
   digitalWrite(fgRotPin, HIGH);
   digitalWrite(fgGruenPin, LOW);
-  delay(5000);
+  delay(10000);
   // gelb
   digitalWrite(ampelRotPin, LOW);
   digitalWrite(ampelGelbPin, HIGH);
   digitalWrite(ampelGruenPin, LOW);
   digitalWrite(fgRotPin, HIGH);
   digitalWrite(fgGruenPin, LOW);
-  delay(1000);
+  delay(2000);
   // rot
   digitalWrite(ampelRotPin, HIGH);
   digitalWrite(ampelGelbPin, LOW);
   digitalWrite(ampelGruenPin, LOW);
   digitalWrite(fgRotPin, LOW);
   digitalWrite(fgGruenPin, HIGH);
-  delay(5000);
+  delay(10000);
   // rot-gelb
   digitalWrite(ampelRotPin, HIGH);
   digitalWrite(ampelGelbPin, HIGH);
   digitalWrite(ampelGruenPin, LOW);
   digitalWrite(fgRotPin, HIGH);
   digitalWrite(fgGruenPin, LOW);
-  delay(1000);
+  delay(2000);
 }
