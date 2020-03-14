e4516224b1daba3ae7378338695731f78bbbc021
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-03-14 16:54:03 +0100

    Update main.cpp

--- Pedestrian_traffic_ light/src/main.cpp
+++ Pedestrian_traffic_ light/src/main.cpp
@@ -1,45 +1,53 @@
 #include <Arduino.h>
-int ampelRotPin = 12;
-int ampelGelbPin = 11;
-int ampelGruenPin = 10;
-int fgRotPin = 9;
-int fgGruenPin = 8;
+int trafficRedPin = 12; //Car red
+int trafficYellowPin = 11; //Car Yellow
+int trafficGreenPin = 10; //Car reen
+int PedRedPin = 9; //Pedestrian red
+int PedGreenPin = 8; //Pedestrian green
 
 void setup() {
-  pinMode(ampelRotPin, OUTPUT);
-  pinMode(ampelGelbPin, OUTPUT);
-  pinMode(ampelGruenPin, OUTPUT);
-  pinMode(fgRotPin, OUTPUT);
-  pinMode(fgGruenPin, OUTPUT);
+  pinMode(trafficRedPin, OUTPUT);
+  pinMode(trafficYellowPin, OUTPUT);
+  pinMode(trafficGreenPin, OUTPUT);
+  pinMode(PedRedPin, OUTPUT);
+  pinMode(PedGreenPin, OUTPUT);
 }
 
 void loop() {
-  // gruen
-  digitalWrite(ampelRotPin, LOW);
-  digitalWrite(ampelGelbPin, LOW);
-  digitalWrite(ampelGruenPin, HIGH);
-  digitalWrite(fgRotPin, HIGH);
-  digitalWrite(fgGruenPin, LOW);
+  // Car green
+  digitalWrite(trafficRedPin, LOW);
+  digitalWrite(trafficYellowPin, LOW);
+  digitalWrite(trafficGreenPin, HIGH);
+  digitalWrite(PedRedPin, HIGH);
+  digitalWrite(PedGreenPin, LOW);
   delay(20000);
-  // gelb
-  digitalWrite(ampelRotPin, LOW);
-  digitalWrite(ampelGelbPin, HIGH);
-  digitalWrite(ampelGruenPin, LOW);
-  digitalWrite(fgRotPin, HIGH);
-  digitalWrite(fgGruenPin, LOW);
+
+  //Pedestrian  red
+  digitalWrite(PedRedPin, HIGH);
+  digitalWrite(PedGreenPin, LOW);
   delay(2000);
-  // rot
-  digitalWrite(ampelRotPin, HIGH);
-  digitalWrite(ampelGelbPin, LOW);
-  digitalWrite(ampelGruenPin, LOW);
-  digitalWrite(fgRotPin, LOW);
-  digitalWrite(fgGruenPin, HIGH);
+  // Car yellow
+  digitalWrite(trafficRedPin, LOW);
+  digitalWrite(trafficYellowPin, HIGH);
+  digitalWrite(trafficGreenPin, LOW);
+  delay(2000);
+  // Car red
+  digitalWrite(trafficRedPin, HIGH);
+  digitalWrite(trafficYellowPin, LOW);
+  digitalWrite(trafficGreenPin, LOW);
+  delay(2000);
+  //Pedestrian green
+  digitalWrite(PedRedPin, LOW);
+  digitalWrite(PedGreenPin, HIGH);
   delay(20000);
-  // rot-gelb
-  digitalWrite(ampelRotPin, HIGH);
-  digitalWrite(ampelGelbPin, HIGH);
-  digitalWrite(ampelGruenPin, LOW);
-  digitalWrite(fgRotPin, HIGH);
-  digitalWrite(fgGruenPin, LOW);
+  //Pedestrian red
+  digitalWrite(PedRedPin, HIGH);
+  digitalWrite(PedGreenPin, LOW);
   delay(2000);
+  // Car red - yellow
+  digitalWrite(trafficRedPin, HIGH);
+  digitalWrite(trafficYellowPin, HIGH);
+  digitalWrite(trafficGreenPin, LOW);
+  delay(2000);
+
 }
