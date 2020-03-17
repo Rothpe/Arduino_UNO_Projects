63c9e48bc78119cdc6be594640aab90d40112e38
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-03-17 17:16:04 +0100

    Update main.cpp

--- Dual_Servo_drive/src/main.cpp
+++ Dual_Servo_drive/src/main.cpp
@@ -1,17 +1,17 @@
  #include <Arduino.h>
  #include <Servo.h>  
 
-Servo servolinks;
-Servo servorechts; 
+Servo servo_left;
+Servo servo_right; 
 
 void setup()
 
 {
 
-servolinks.attach(8);
-servorechts.attach(9);
-servolinks.write(0);
-servorechts.write(180);
+servo_left.attach(8);
+servo_right.attach(9);
+servo_left.write(0);
+servo_right.write(180);
 
 } 
 
@@ -19,13 +19,13 @@ void loop() {
 
   for (int i = 0; i < 90; i++)
   {
-    servolinks.write(i);
+    servo_left.write(i);
     delay(8);
   }
 
   for (int j = 180; j > 90; j--)
   {
-    servorechts.write(j);
+    servo_right.write(j);
     delay(8);
   }
   delay(3000);
@@ -33,13 +33,13 @@ void loop() {
 
   for (int i = 90; i > 0; i--)
   {
-    servolinks.write(i);
+    servo_left.write(i);
     delay(8);
   }
 
   for (int j = 90; j < 180; j++)
   {
-    servorechts.write(j);
+    servo_right.write(j);
     delay(8);
   }
   delay(3000);
