6623a431444c3968371b5ad6ba09fbd1df1714c1
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-03-17 17:53:37 +0100

    Update main.cpp

--- Dual_Servo_drive/src/main.cpp
+++ Dual_Servo_drive/src/main.cpp
@@ -20,13 +20,13 @@ void loop() {
   for (int i = 0; i < 90; i++)
   {
     servo_left.write(i);
-    delay(8);
+    delay(4);
   }
 
   for (int j = 180; j > 90; j--)
   {
     servo_right.write(j);
-    delay(8);
+    delay(4);
   }
   delay(3000);
 
@@ -34,13 +34,13 @@ void loop() {
   for (int i = 90; i > 0; i--)
   {
     servo_left.write(i);
-    delay(8);
+    delay(4);
   }
 
   for (int j = 90; j < 180; j++)
   {
     servo_right.write(j);
-    delay(8);
+    delay(4);
   }
   delay(3000);
   
