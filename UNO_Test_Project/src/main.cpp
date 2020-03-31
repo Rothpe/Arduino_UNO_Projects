540a892d4d610eeaf2950015b950e7c4ad84cf27
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-03-31 18:40:34 +0200

    Update main.cpp

--- UNO_Test_Project/src/main.cpp
+++ UNO_Test_Project/src/main.cpp
@@ -8,7 +8,7 @@ void setup()
 {
 
 servo.attach(8);
-servo.write(90);
+servo.write(90); //hier wird Pos 90° angefahren
 
 } 
 
@@ -16,7 +16,7 @@ void loop() {
 
   delay(20000);
 
-  for (int i = 0; i < 90; i++)
+  for (int i = 0; i < 90; i++) //hier fahren wir von 90° zu 0°
   {
     servo.write(i);
     delay(12);
@@ -24,7 +24,22 @@ void loop() {
   delay(3000);
 
 
-  for (int i = 90; i > 0; i--)
+  for (int i = 90; i > 0; i--) //hier fahren wir wieder auf 90° zurück
+  {
+    servo.write(i);
+    delay(12);
+  }
+  delay(3000);
+
+  for (int i = 0; i < 180; i++) //hier fahren wir von 90° auf 270° --> 90° + 180°
+  {
+    servo.write(i);
+    delay(12);
+  }
+  delay(3000);
+
+
+  for (int i = 180; i > 0; i--) //hier fahren wir wieder auf 90° zurück --> 270° - 180°
   {
     servo.write(i);
     delay(12);
