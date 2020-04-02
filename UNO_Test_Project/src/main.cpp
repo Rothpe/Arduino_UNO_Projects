bc8703b8db1f8e87a003a07e7aeb807844e10cd2
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-04-02 14:39:57 +0200

    Update main.cpp

--- UNO_Test_Project/src/main.cpp
+++ UNO_Test_Project/src/main.cpp
@@ -1,49 +1,15 @@
  #include <Arduino.h>
- #include <Servo.h>  
+ 
+ void setup(){
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
 
-Servo servo;
-
-void setup()
-
-{
-
-servo.attach(8);
-servo.write(90); //hier wird Pos 90° angefahren
-
-} 
-
-void loop() {
-
-  delay(20000);
-
-  for (int i = 0; i < 90; i++) //hier fahren wir von 90° zu 0°
-  {
-    servo.write(i);
-    delay(12);
-  }
-  delay(3000);
-
-
-  for (int i = 90; i > 0; i--) //hier fahren wir wieder auf 90° zurück
-  {
-    servo.write(i);
-    delay(12);
-  }
-  delay(3000);
-
-  for (int i = 0; i < 180; i++) //hier fahren wir von 90° auf 270° --> 90° + 180°
-  {
-    servo.write(i);
-    delay(12);
-  }
-  delay(3000);
-
-
-  for (int i = 180; i > 0; i--) //hier fahren wir wieder auf 90° zurück --> 270° - 180°
-  {
-    servo.write(i);
-    delay(12);
-  }
-  delay(3000);
-  
+void loop(){
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(1000);
 }
