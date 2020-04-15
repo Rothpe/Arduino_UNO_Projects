e718b8d327b708c5fbfe69cdeeb41004bc1b06b3
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-04-15 16:45:49 +0200

    Update main.cpp

--- Time_Model_Train/src/main.cpp
+++ Time_Model_Train/src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
 #include <time.h>
 
-int hour = 14;
-int min = 0;
-int sec = 0;
+int h,m,s;
+int set_hours=20;
+int set_minutes=49;
+int set_seconds=45;
+int secs=1000;
 
 void setup() {
   Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  for(h=set_hours;h<24;h++)   //Hours
+  {
+    for(m=set_minutes;m<60;m++)   //Minutes
+    {
+      for(s=set_seconds;s<60;s++)     //Seconds
+      {
+        Serial.println(m);
+        delay(secs-(millis()%1000));
+      }
+      set_seconds=00;
+    }
+    set_minutes=00;
+  }
+  set_hours=00;
 }
