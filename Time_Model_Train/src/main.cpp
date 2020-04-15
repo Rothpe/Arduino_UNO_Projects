0e283673020e990f06aa2b167da889a4ab5b88fa
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-04-15 16:53:16 +0200

    Update main.cpp

--- Time_Model_Train/src/main.cpp
+++ Time_Model_Train/src/main.cpp
@@ -2,27 +2,27 @@
 #include <time.h>
 
 int h,m,s;
-int set_hours=20;
-int set_minutes=49;
-int set_seconds=45;
+int hours=20;
+int minutes=49;
+int seconds=45;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop(){
-  for(h=set_hours;h<24;h++)   //Hours
+  for(h=hours;h<24;h++)   //Hours
   {
-    for(m=set_minutes;m<60;m++)   //Minutes
+    for(m=minutes;m<60;m++)   //Minutes
     {
-      for(s=set_seconds;s<60;s++)     //Seconds
+      for(s=seconds;s<60;s++)     //Seconds
       {
         Serial.println(h);
         delay(1000);
       }
-      set_seconds=00;
+
     }
-    set_minutes=00;
+    
   }
-  set_hours=00;
+  
 }
