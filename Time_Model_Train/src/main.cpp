92815b33231b507104e403b642ab69eaa7ac531c
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-04-15 15:21:32 +0200

    new

---
+++ Time_Model_Train/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <time.h>
+
+int hour = 14;
+int min = 0;
+int sec = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
