ef3c94ac81ecb5db707c56654e62ceef15d95ae8
Rothpe <50524413+Rothpe@users.noreply.github.com>
2020-04-27 18:43:04 +0200

    update

--- UNO_Test_Project/src/main.cpp
+++ UNO_Test_Project/src/main.cpp
@@ -1,44 +1,17 @@
 #include <Arduino.h>
+int digitalPin = 8; // linear Hall digital interface
+int digitalVal;     // digital readings
 
-#define PIN_LED 10                         // Pin, an dem die LED angeschlossen ist.
-#define PIN_BUTTON 9                      // Pin, an dem der Taster angeschlossen ist.
-
-boolean aLedEnabled = false;                                        // True, wenn die LED leuchtet, sonst false.
-boolean aButtonPressed = false;                                     // True, wenn der Taster gedrÃ¼ckt wird, sonst false.
-
-void setup()
+void setup ()
 {
-  pinMode(PIN_LED, OUTPUT);                                         // Pin, an dem die LED angeschlossen ist, als Ausgang festlegen.
-  pinMode(PIN_BUTTON, INPUT_PULLUP);                                // Pin, an dem der Taster angeschlossen ist, als Eingang mit aktiviertem Pull-Up-Widerstand festlegen.
+  pinMode(digitalPin, INPUT); 
+  Serial.begin(9600);
 }
 
-void loop()
+void loop ()
 {
-  if (digitalRead(PIN_BUTTON) == LOW) {                             // Wenn der Taster aktuell gedrÃ¼ckt wird, ...
-    aButtonPressed = true;                                          // merke dir, dass der Taster gedrÃ¼ckt wurde, ...
-    delay(10);                                                      // und warte 10 Millisekunden bis die Kontakte des Tasters vollstÃ¤ndig geschlossen sind.
-  }
-
-  if (digitalRead(PIN_BUTTON) == HIGH && aButtonPressed == true) {  // Wenn der Taster nicht gedrÃ¼ckt wird, aber zuvor gedrÃ¼ckt wurde ...
-    aButtonPressed = false;                                         // vergesse, dass der Taster gedrÃ¼ckt wude.
-    
-    if (aLedEnabled == true) {                                      // Wenn die LED gerade leuchtet ...
-      digitalWrite(PIN_LED, LOW);                                   // schalte die LED aus ...
-      aLedEnabled = false;                                          // und merke dir, dass die LED gerade nicht leuchtet,
-    } else {                                                        // sonst ...
-      digitalWrite(PIN_LED, HIGH);  
-      delay(300); 
-      digitalWrite(PIN_LED, LOW);  
-      delay(300);        
-      digitalWrite(PIN_LED, HIGH);  
-      delay(300);        
-      digitalWrite(PIN_LED, LOW); 
-      delay(500);
-      digitalWrite(PIN_LED,HIGH) ;
-      delay(2000);
-      digitalWrite(PIN_LED, LOW); 
-                                         // schalte die LED ein ...
-      aLedEnabled = true;                                           // und merke dir, dass die LED gerade leuchtet.
-    }
-  }
+  // Read the digital interface
+  digitalVal = digitalRead(digitalPin);
+  Serial.println(digitalVal); // print digital value
+  delay(100);
 }
